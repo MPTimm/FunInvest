6cf231c8e81b8fa9c6c16dcc78c0598c9b1dc184
Matheus <matheusptimm@gmail.com>
2019-06-16 19:02:38 -0300

    JUnit

---
+++ src/TesteTesouroDiretoJUnit.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class TesteTesouroDiretoJUnit {
+	@Test
+	public void testeTD() {
+		InvestimentoFactory investimentoFactory = new InvestimentoFactory();
+
+		//Instancia de TesouroDireto usando Factory
+		Investimento tesouro = investimentoFactory.getInvestimento("TesouroDireto");
+		
+		tesouro.investir(150);
+		tesouro.investir(340);
+		tesouro.investir(1500);
+		
+		//Previsão de resgate do montante investido em TesouroDireto
+		tesouro.previous(5);
+	}
+}
